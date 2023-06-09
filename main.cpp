2f0972a4b129a0d73efb7dc0bb43eeff59136219
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-06-09 23:55:07 +0530

    kinda works
    button pops and loop works

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ bool b7 = false;
 bool b8 = false;
 bool b9 = false;
 int choice;
+int button_to_pop;
 std::string block = "██";
 
 void draw_board(){
@@ -21,9 +22,136 @@ void draw_board(){
     std::cout << "\x1b[91m" << block;
   }
   if(b2==0){
-    
+    std::cout << "\x1b[33m" << block;
   }
-  
+  else{
+    std::cout << "\x1b[93m" << block;
+  }
+  if(b3==0){
+    std::cout << "\x1b[34m" << block << "\x1b[0m" << std::endl;
+  }
+  else{
+    std::cout << "\x1b[94m" << block << "\x1b[0m" << std::endl;
+  }
+  if(b4 == 0){
+   std::cout << "\x1b[31m" << block;
+  }
+  else{
+    std::cout << "\x1b[91m" << block; 
+  }
+  if(b5 == 0){
+    std::cout << "\x1b[33m" << block;
+  }
+  else{
+    std::cout << "\x1b[93m" << block;
+  }
+  if(b6 == 0){
+    std::cout << "\x1b[34m" << block << std::endl;
+  }
+  else{
+    std::cout << "\x1b[94m" << block << std::endl;
+  }
+  if(b7 == 0){
+    std::cout << "\x1b[31m" << block;
+  }
+  else{
+    std::cout << "\x1b[91m" << block;
+  }
+  if(b8 == 0){
+    std::cout << "\x1b[33m" << block;
+  }
+  else{
+    std::cout << "\x1b[93m" << block;
+  }
+  if(b9==0){
+    std::cout << "\x1b[34m" << block << "\x1b[0m" << std::endl;
+  }
+  else{
+    std::cout << "\x1b[94m" << block << "\x1b[0m" << std::endl;
+  }
+  //get_input();
+}
+
+void get_input(){
+  //button_to_pop = 0;
+  std::cout << "enter input between[1-9] to pop: ";
+  std::cin >> button_to_pop;
+  if(button_to_pop==1){
+    if(b1==0){
+      b1 = true;
+    }
+    else{
+      b1 = false;
+    }
+  }
+  else if(button_to_pop==2){
+    if(b2==0){
+      b2 = true;
+    }
+    else{
+      b2 = false;
+    }
+  }
+  else if(button_to_pop==3){
+      if(b3==0){
+        b3 = true;     
+      }
+      else{
+        b3 = false;
+      }
+  }
+  else if(button_to_pop==4){
+      if(b4==0){
+        b4 = true;     
+      }
+      else{
+        b4 = false;
+      }
+  }
+  else if(button_to_pop==5){
+    if(b5==0){
+      b5 = true;     
+    }
+    else{
+      b5 = false;
+    }
+  }
+  else if(button_to_pop==6){
+    if(b6==0){
+      b6 = true;     
+    }
+    else{
+      b6 = false;
+    }
+  }
+  else if(button_to_pop==7){
+    if(b7==0){
+      b7 = true;     
+    }
+    else{
+      b7 = false;
+    }
+  }
+  else if(button_to_pop==8){
+    if(b8==0){
+      b8 = true;     
+    }
+    else{
+      b8 = false;
+    }
+  }
+  else if(button_to_pop==9){
+    if(b9==0){
+      b9 = true;     
+    }
+    else{
+      b9 = false;
+    }
+  }
+  else{
+    std::cout << "\x1b[31merror: invalid choice\x1b[0m" << std::endl;
+  }
+  draw_board();
 }
 
 int main(){
@@ -35,8 +163,12 @@ int main(){
   switch(choice){
     case 1:
       draw_board();
+      while(choice==1){
+        get_input();
+      }
+      return 0;
     case 2:
-      std::cout << "\x1b[33mnote: user chose to exit so exiting" << std::endl;
+      std::cout << "\x1b[33mnote: user chose to exit so exiting\x1b[0m" << std::endl;
       return 0;
     default:
       std::cout << "\x1b[31merror: invalid choice so exiting\x1b[0m" << std::endl;
